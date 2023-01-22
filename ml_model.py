10b61e5f1678e977088df062da7e3e09239e8811
zaksha <shaikhzakir.qa@gmail.com>
2023-01-22 15:06:13 +0300

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,13 @@
 print("Adding all ml related code here")
+
+
+def model1():
+    print("adding code for ML Model1")
+
+def model2():
+    print("adding code for ML Model2")
+
+def model3():
+    print( a"adding code for ML Model3")
+
+    
